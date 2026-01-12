ce951dc52865397f3bc09bfa9d0133b002c9957f
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-12 20:18:43 +0530

    Add Social Experiment [A] from Codeforces

---
+++ 2184/A - Social Experiment/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	// your code goes here	int T ; 	cin >> T ; 		while(T --){	    int n ; 	    cin >> n ; 	    	    if(n == 2 || n == 3){	        cout << n << endl;	        continue ;	    }	    	    if(n & 1){	        cout << 1 << endl;	    }else{	        cout << 0 << endl;	    }	}		return 0; }

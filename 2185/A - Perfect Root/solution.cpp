e91c793c064c73a80df9ee9204f345c922326a7f
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-18 20:23:08 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	// your code goes here	int T;	cin >> T;		while(T --){	     int n;        cin >> n;        for (int i = 1; i <= n; i++) {            cout << 1LL * i * i << " ";        }        cout << '\n';	}		return 0; }

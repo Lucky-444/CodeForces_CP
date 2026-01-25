6dc153833a602714ac0ce876f713a8ea65ccbd27
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-25 18:13:12 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	// your code goes here	int n ;	cin >> n ; 		vector<int>vec(n);	int mini = INT_MAX;		for(int i = 0 ; i < n ; i ++){	    cin >> vec[i];	    	    mini = min(mini , abs(vec[i]));	    	}		cout << mini << endl ; }

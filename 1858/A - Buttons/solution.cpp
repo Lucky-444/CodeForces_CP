d5a87b0d026b7bf00799788dcd632c1e2fd83e48
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-26 12:07:46 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;using ll = long long ; int main() {	// your code goes here		ll t; 		cin >> t ; 		while(t --){	    int a , b , c ;	    cin >> a  >> b >> c ; 	    	    if (a > b) cout << "First\n";        else if (a < b) cout << "Second\n";        else cout << (c % 2 ? "First\n" : "Second\n");	}		return 0 ; }

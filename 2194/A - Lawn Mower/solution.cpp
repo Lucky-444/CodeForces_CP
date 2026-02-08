ab9b87125e46e77e764da49329babc1a015ea410
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-08 23:34:48 +0530

    Add Lawn Mower [A] from Codeforces

---
+++ 2194/A - Lawn Mower/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;using ll = long long; int main() {    ll t;    cin >> t;                      while (t--) {        ll n, w;        cin >> n >> w;         ll ans = n - (n / w);                cout << ans << endl;    }}

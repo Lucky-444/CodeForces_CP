b0a2eb72c891caa18f68a986f79285cce98ac0f7
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-03-27 10:50:23 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    int t;    cin >> t;        while (t--) {        int n;        cin >> n;                vector<int> a(n);        for (auto &x : a) cin >> x;                // Sort in descending order        sort(a.rbegin(), a.rend());                for (auto x : a) cout << x << " ";        cout << endl;    }}

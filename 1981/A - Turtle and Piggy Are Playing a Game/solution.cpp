0010e98ce39951cbcf0ea322c01e0c3e6825f136
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-07-02 20:30:14 +0530

    Add Turtle and Piggy Are Playing a Game [A] from Codeforces

---
+++ 1981/A - Turtle and Piggy Are Playing a Game/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+void solve(){
+   int l,r;
+   cin>>l>>r;
+   
+     
+    int ans = log2(r);  // Use log2() instead of log() for base-2 logarithm
+ 
+    cout << ans << endl;
+   
+  
+   
+}
+int main() {
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+ 
+    
